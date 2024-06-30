05314fb5c6cdea9def2e6c7331e4fabf52db491b
Greg Robleto <greg@robleto.com>
2024-06-30 01:06:39 -0400

    fixing up the footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,10 +1,65 @@
 import React from "react";
 import styles from "../styles/Footer.module.css";
 
+const footerIcons = [
+	{
+		alt: "Website",
+		src: "/images/_icons/website-icon.svg",
+		href: "https://www.robleto.com",
+	},
+	{
+		alt: "CodePen",
+		src: "/images/_icons/codepen-icon.svg",
+		href: "https://www.codepen.com/robleto",
+	},
+	{
+		alt: "Dribbble",
+		src: "/images/_icons/dribbble-icon.svg",
+		href: "https://www.dribbble.com/robleto",
+	},
+	{
+		alt: "GitHub",
+		src: "/images/_icons/github-icon.svg",
+		href: "https://www.github.com/robleto",
+	},
+	{
+		alt: "LinkedIn",
+		src: "/images/_icons/linkedin-icon.svg",
+		href: "https://www.linkedin.com/in/robleto",
+	},
+];
+
 const Footer: React.FC = () => (
 	<footer className={styles.footer}>
-		<p>Inspired by the original, now defunct, Star Wars Weather</p>
-		<p>Designed and developed by Greg Robleto</p>
+		<p className={styles.inspiredText}>
+			Inspired by a previous{" "}
+			<a
+				href="https://www.tomscott.com/weather/starwars/"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				Star Wars Weather
+			</a>.
+		</p>
+		<div className={styles.developedBy}>
+			<p>Designed / developed by Greg Robleto</p>
+			<span className={styles.iconContainer}>
+				{footerIcons.map((icon) => (
+					<a
+						key={icon.alt}
+						href={icon.href}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<img
+							src={icon.src}
+							alt={icon.alt}
+							className={styles.socialIcon}
+						/>
+					</a>
+				))}
+			</span>
+		</div>
 	</footer>
 );
 
